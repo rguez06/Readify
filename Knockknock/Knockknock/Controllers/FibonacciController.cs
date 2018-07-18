3e83e13ead73c15d14cfe40c3e3374673da4ccce
Ryan Rodriguez <rguez_18@yahoo.com>
2018-07-18 12:03:12 +1000

    Initial Add

---
+++ Knockknock/Knockknock/Controllers/FibonacciController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestSharp;
+namespace Knockknock.Controllers
+{
+    public class FibonacciController : ApiController
+    {
+        // GET: api/Fibonacci
+        public int Get()
+        {
+            return 0;
+        }
+
+        // GET: api/Fibonacci/5
+        public long Get(int n)
+        {
+            long result = 0;
+            var client = new RestClient("http://knockknock.readify.net/api/Fibonacci?n=" + n.ToString());
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "9cff5628-6d5c-48e8-5bab-7788fe91378d");
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
+
+            long.TryParse(response.Content.ToString(), out result);
+            return result;
+        }
+
+        // POST: api/Fibonacci
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Fibonacci/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Fibonacci/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Knockknock/Knockknock/Controllers/TriangleTypeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestSharp;
+
+namespace Knockknock.Controllers
+{
+    public class TriangleTypeController : ApiController
+    {
+        // GET: api/TriangleType
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TriangleType/5
+        public string Get(int a, int b, int c)
+        {
+            string result = string.Empty;
+            var client = new RestClient(string.Format("http://knockknock.readify.net/api/TriangleType?a={0}&b={1}&c={2}", a.ToString(), b.ToString(), c.ToString()));
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "9cff5628-6d5c-48e8-5bab-7788fe91378d");
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
+
+
+            return response.Content.ToString();
+        }
+
+        // POST: api/TriangleType
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TriangleType/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TriangleType/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
